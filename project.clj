15919471b41b7bdad4f50ce89b55dd32cf44db01
Robert J. Lambert III <rob@rlambert3.com>
2015-02-14 14:08:00 -0500

    Moved the resources to public, split the src to client and server added cljs to project

--- project.clj
+++ project.clj
@@ -3,14 +3,26 @@
   :url "https://github.com/jvalentini/mtg-proxy-pdf"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+
+  :source-paths ["src/clj"]
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [enlive "1.1.5"]
                  [clj-pdf "1.11.15"]
                  [ring/ring-codec "1.0.0"]
                  [hiccup "1.0.5"]
                  [org.clojure/data.json "0.2.4"]
-                 [org.clojure/tools.cli "0.3.1"]]
-  :profiles { :dev {:dependencies [[org.clojure/tools.trace "0.7.6"]
-                                   [midje "1.5.1"]]}}
+                 [org.clojure/tools.cli "0.3.1"]
+                 [org.clojure/clojurescript "0.0-2850"]]
+
+  :plugins [[lein-cljsbuild "1.0.3"]]
+
+  :cljsbuild {:builds [{:source-paths ["src/cljs"]
+                        :compiler {:output-to "resources/public/js/client.js"
+                                   :optimizations :none
+                                   :source-map true}}]}
+
+  :profiles { :dev {:dependencies [[org.clojure/tools.trace "0.7.6"]]}}
+
   :main mtg-proxy-pdf.core
   :aot [mtg-proxy-pdf.core])
---
+++ src/cljs/mtg_proxy_pdf/client.cljs
@@ -0,0 +1,3 @@
+(ns mtg-proxy-pdf.client)
+
+  (.write js/document "<h1>MTG Proxy</h1>")
